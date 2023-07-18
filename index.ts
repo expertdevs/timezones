f0ca083e50a0066e4d6c9e8622cecaf8e25475f5
Manoj Sethi <manojsethi@live.in>
2023-07-18 10:57:57 +0530

    M.Sethi => API Changes

--- index.ts
+++ index.ts
@@ -1,13 +1,25 @@
-import { getTimeZones } from "@vvo/tzdb";
+import { getTimeZones, TimeZone } from "@vvo/tzdb";
 import express, { Request, Response } from "express";
 import Fuse from "fuse.js";
+import _ from "lodash";
 import moment from "moment-timezone";
-
+import countries from "world-countries";
 const app = express();
 app.use(express.json());
+interface BCTimezone extends TimeZone {
+  latLng?: [number, number];
+}
 // Endpoint to get the list of timezones
 app.get("/timezones", (_req, res) => {
-  const timezones = getTimeZones();
+  const timezones: BCTimezone[] = getTimeZones();
+  timezones.forEach((tz) => {
+    let countryCode = tz.countryCode;
+    let countryName = tz.countryName;
+    let wCountry = countries.find(
+      (x) => x.name.common == countryName || x.cca2 == countryCode
+    );
+    if (wCountry) tz.latLng = wCountry.latlng;
+  });
   res.json(timezones);
 });
 
@@ -21,33 +33,25 @@ app.get("/timezones-autocomplete", (req, res) => {
       "countryCode",
       "abbreviation",
     ],
-    threshold: 0.6, // Adjust the threshold to control fuzziness
+    threshold: 0.3, // Adjust the threshold to control fuzziness
   };
-  const timezones = getTimeZones();
+
+  const timezones: BCTimezone[] = getTimeZones();
   // Create a Fuse instance with the timezones data and options
   const fuse = new Fuse(timezones, fuseOptions);
   const searchQuery = req.query.q as string;
   const results = fuse.search(searchQuery);
-  res.json(results);
-});
-
-//Endpoint to convert current UTC time to specified timezones
-app.post("/convert-time", (req, res) => {
-  const { timezone } = req.body;
-  const utcMoment = moment.utc();
-  const convertedTimes = Array.isArray(timezone)
-    ? timezone.map((tz) => {
-        const convertedTime = utcMoment.tz(tz).format("YYYY-MM-DD HH:mm:ss");
-        return { tz, convertedTime };
-      })
-    : [
-        {
-          tz: timezone,
-          convertedTime: utcMoment.tz(timezone).format("YYYY-MM-DD HH:mm:ss"),
-        },
-      ];
-
-  res.json({ convertedTimes });
+  results.forEach((tz) => {
+    let countryCode = tz.item.countryCode;
+    let countryName = tz.item.countryName;
+    let wCountry = countries.find(
+      (x) => x.name.common == countryName || x.cca2 == countryCode
+    );
+    if (wCountry) tz.item.latLng = wCountry.latlng;
+  });
+  let finalResults = results.map((x) => x.item);
+  finalResults = _.uniqBy(finalResults, "countryName");
+  res.json(finalResults);
 });
 
 //Endpoint to convert current UTC time to specified timezones
